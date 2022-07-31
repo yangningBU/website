d644c0fc986a88832072daa971ec1f7a9179f19f
Yonatan Laurence <laurence0521@gmail.com>
2022-07-31 18:59:25 +0300

    Actually update the language section

--- src/components/SiteLinks/SiteLinks.js
+++ src/components/SiteLinks/SiteLinks.js
@@ -12,7 +12,7 @@ const Icon = (props) => {
 
 const openPDF = (event) => {
   event.preventDefault();
-  window.open('../../assets/files/YonatanLaurence_Resume_2022-07_06.pdf');
+  window.open('../../assets/files/YonatanLaurence_Resume_2022-07_07.pdf');
 }
 
 const SiteLinks = () => (
